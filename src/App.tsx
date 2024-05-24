1dd5a908eea7901a895afd9d1ed29cd405f83597
Abilio2122 <ruben.carmun@gmail.com>
2024-05-24 12:49:37 -0400

    ver si funciona

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
-import Tab4 from './pages/tab4';
+import Tab4 from './pages/inicio_sesion';
 import Tab5 from './pages/registro';
 
 /* Core CSS required for Ionic components to work properly */
